22a5eca140b3a1fbaed8d27efc92916eb2119910
dyjeong576 <dyjeong576@gmail.com>
2022-10-19 17:45:10 +0900

    Open API 3.0 Spec (swagger) 구현

--- public/app.js
+++ public/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const { globalErrorHandler } = require('./src/utils/error');    
+const { globalErrorHandler } = require('./src/utils/error');
+const { swaggerUi, specs } = require('./src/swagger/swagger');   
+
 const routes = require("./src/routers");
+
 
 const createApp = () => {
   const app = express();
@@ -10,6 +13,7 @@ const createApp = () => {
   app.use(express.json());
   app.use(cors());
   app.use(morgan("dev"));
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
   app.get('/ping', function (req, res, next) {
     res.json({message : 'pong'})
--- public/src/routers/index.js
+++ public/src/routers/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router();
 
-newsRouter = require('./newsRouter');
+newsRouter = require('./newsRouter')
 
 router.use('/news', newsRouter);
 
